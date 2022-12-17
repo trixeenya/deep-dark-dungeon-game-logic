7af45a901577052b7aa52855c62c88771a3f9a31
TriXee <trixeenya@gmail.com>
2022-12-17 23:54:29 +0200

    s

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,1 @@
-import 'package:dep_dark_dungeon_game_logic/dep_dark_dungeon_game_logic.dart' as dep_dark_dungeon_game_logic;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dep_dark_dungeon_game_logic.calculate()}!');
-}
+void main(List<String> arguments) {}
