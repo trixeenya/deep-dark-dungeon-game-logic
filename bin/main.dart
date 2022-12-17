d3b98af836e6e7fc882bc9227091eb9512d13711
TriXee <trixeenya@gmail.com>
2022-12-17 23:53:50 +0200

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dep_dark_dungeon_game_logic/dep_dark_dungeon_game_logic.dart' as dep_dark_dungeon_game_logic;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dep_dark_dungeon_game_logic.calculate()}!');
+}
